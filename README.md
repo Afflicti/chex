51c7d824d36890695d8d3d481a0b92b9505ac52c
Jakub Kopčanský <kuba.kopcansky@gmail.com>
2026-01-23 22:48:19 +0100

    code here, it works somehow

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # chex
-chex is a tool to to quickly view .hex files in terminal
+chex is a tool to quickly view .hex files in terminal
+
+## TODO:
+- create help -h 
+- create option to add start and end address to display only part of hex file
+- add coloring of characters (hex codes of characers)
+- check if checksum is correct
+- print "metadata" of the hexfile
---
+++ src/main.c
@@ -0,0 +1,211 @@
+#include <stdio.h>
+#include <string.h> 
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+// preparation for coloring
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+// error enum
+enum response
+{
+    OK = 0,         // ALL OK
+    NOT_OK = 1,     // GENERAL ERROR
+};
+
+// struct with data received from call parameters
+struct data
+{
+    char filepath[256];
+    bool hasStart;
+    unsigned long startAddres;
+    bool hasEnd;
+    unsigned long endAddres;
+};
+
+// i was supposed to use this somewhere, but I did not... ¯\_(ツ)_/¯
+bool file_exists(const char *filename)
+{
+    struct stat buffer;
+    return stat(filename, &buffer) == 0 ? true : false;
+}
+
+// parse args
+int parseArguments(int argc, char **argv, struct data *currentData)
+{
+    printf("Number of Arguments: %d, Arguments: ", argc);
+    for (int i = 0; i < argc; i++ )
+    {
+        printf("%s ", argv[i]);
+    }
+    printf("\n");
+
+    if (argc == 2 && strcmp(argv[1], "-h") == 0)
+    {
+        printf("TODO PRINT HELP\n");
+        return NOT_OK;
+    }
+
+    if (argc == 1)
+    {
+        printf("No arguments provided. Try -h for help.\n");
+        return NOT_OK;
+    }
+
+    if (argc % 2 == 0)
+    {
+        printf("Wrong number of arguments. Try -h for help.\n");
+        return NOT_OK;
+    }
+
+    for (int i = 1; i < argc; i=i+2 )
+    {
+        if (strcmp(argv[i], "-f") == 0)
+        {
+            printf("File to read: %s \n", argv[i+1]);
+
+            strcpy(currentData->filepath, argv[i+1]);
+        }
+        else if (strcmp(argv[i], "-s") == 0)
+        {
+            printf("Start addres: %s \n", argv[i+1]);
+
+            currentData->hasStart = true;
+            currentData->startAddres = strtol(argv[i+1], NULL, 16);
+        }
+        else if (strcmp(argv[i], "-e") == 0)
+        {
+            printf("End addres: %s \n", argv[i+1]);
+
+            currentData->hasEnd = true;
+            currentData->endAddres = strtol(argv[i+1], NULL, 16);
+        }
+        else
+        {
+            printf("Unknown argument. Try -h for help.\n");
+            return NOT_OK;
+        }
+    } 
+    
+    return OK;
+}
+
+// display the line of hex, but make it nice
+int displayLine(char *line)
+{
+    char strByte[2];
+    long int DataLen = 0;
+
+    // byte count
+    memcpy(strByte, &line[1], 2);
+    DataLen = strtol(strByte, NULL, 16);
+    printf("%02X", DataLen);
+
+    // address
+    memcpy(strByte, &line[3], 2);
+    printf(" %02X", strtol(strByte, NULL, 16));
+    memcpy(strByte, &line[5], 2);
+    printf("%02X", strtol(strByte, NULL, 16));
+
+    // record type
+    memcpy(strByte, &line[7], 2);
+    printf(" %02X | ", strtol(strByte, NULL, 16));
+
+    // data
+    for (int i = 9; i < (DataLen * 2) + 8; i = i + 2)
+    {
+        memcpy(strByte, &line[i], 2);
+        printf("%02X ", strtol(strByte, NULL, 16));
+
+    }
+
+    // fill the rest data empty
+    for (int i = (DataLen * 2) + 8; i < 40; i = i + 2)
+    {
+        memcpy(strByte, &line[i], 2);
+        printf("   ", strtol(strByte, NULL, 16));
+    }
+
+    // CS
+    memcpy(strByte, &line[41], 2);
+    printf("| %02X || ", strtol(strByte, NULL, 16));
+
+    for (int i = 9; i < (DataLen * 2) + 8; i = i + 2)
+    {
+        memcpy(strByte, &line[i], 2);
+        long int character = 0;
+        character = strtol(strByte, NULL, 16);
+
+        //skip problematic chars
+        if ((character >= 0x00 && character <= 0x20) || character == 0x7F || character == 0xFF || character == 0xF0)
+        {
+            printf("  ");
+        }
+        else
+        {
+            printf("%c ", character);
+        }
+    }
+
+    // fill the rest cahracters empty
+    for (int i = (DataLen * 2) + 8; i < 40; i = i + 2)
+    {
+        memcpy(strByte, &line[i], 2);
+        printf("  ", strtol(strByte, NULL, 16));
+    }
+
+    printf("|");
+    printf("\n");
+
+    return OK;
+}
+
+// reads lines of hex file one by one and display content to terminal
+int displayContentFromData(struct data *currentData)
+{
+    FILE *fptr;
+    fptr = fopen(currentData->filepath, "r");
+
+    char line[45]; // one line should have 44 characters + endline character
+
+    if (fptr != NULL) {
+
+        while (fgets(line, sizeof(line), fptr)) 
+        {
+            displayLine(line);
+            memset(line, 0x00, 45);
+        }
+
+        fclose(fptr);
+    }
+
+    return OK;
+}
+
+// main...
+int main(int argc, char **argv) 
+{
+    struct data currentData = {"", false, 0x00, false, 0x00};
+
+    if (parseArguments(argc, argv, &currentData) != OK)
+    {
+        return 0;
+    }
+    
+    if (displayContentFromData(&currentData) != OK)
+    {
+        return 0;
+    }
+
+    return 0;
+}
+
+
